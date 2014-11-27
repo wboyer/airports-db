28171c2fe6da2670f7cc218b66f61fe0812ec1fc
Bill Boyer <william.boyer.nyc@gmail.com>
2014-11-27 13:45:05 -0500

    Eliminated dups and nulls from the index.  Also now indexing on city and country.

--- src/db/index-airports-collection.js
+++ src/db/index-airports-collection.js
@@ -23,6 +23,9 @@ db.runCommand(
         {
             function emitPrefixes(str, airport)
             {
+				if (!str)
+					return;
+
                 var substrings = [];
                 var regexp = /\w+/g;
                 var word;
@@ -36,8 +39,21 @@ db.runCommand(
 
                 for (s in substrings)
                     for (var i = 1; (i < 10) && (i <= substrings[s].length); i++) {
-                        var key = JSON.parse('{"prefix" : "' + substrings[s].substring(0, i).toLowerCase() + '"}');
-                        var value = JSON.parse('{"matches": [{"code": "' + airport.code + '","name": "' + airport.name + '","city": "' + airport.city + '","country": "' + airport.country + '"}]}');
+                        var key = {"prefix": substrings[s].substring(0, i).toLowerCase()};
+
+                        var match = {"code": airport.code};
+
+                        if (airport.name)
+                            match.name = airport.name;
+
+                        if (airport.city)
+                            match.city = airport.city;
+
+                        if (airport.country)
+                            match.country = airport.country;
+
+                        var value = { "matches": [ match ]};
+
                         emit(key, value);
                     }
 
@@ -45,16 +61,30 @@ db.runCommand(
 
             emitPrefixes(this.code, this);
             emitPrefixes(this.name, this);
+            emitPrefixes(this.city, this);
+            emitPrefixes(this.country, this);
         },
 
         reduce: function (key, values)
         {
             var matches = [];
+			var uniqueCodes = {};
 
             for (var i in values) {
                 var value = values[i];
-                for (var j in value.matches)
-                    matches.push(value.matches[j]);
+
+                for (var j in value.matches) {
+	                var match = value.matches[j];
+                    if (!match)
+                        continue;
+
+					var code = match.code;
+					if (uniqueCodes[code])
+                        continue;
+                    uniqueCodes[code] = true;
+
+                    matches.push(match);
+				}
             }
 
             return { "matches": matches };
